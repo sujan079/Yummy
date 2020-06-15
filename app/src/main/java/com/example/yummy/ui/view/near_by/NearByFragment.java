0c62ba33d5d54181bd2c5cf9181e6e17ea61bffb
sujan079 <sujantimalsina079@gmail.com>
2020-06-15 20:37:56 +0545

    "Changes"

--- app/src/main/java/com/example/yummy/ui/view/near_by/NearByFragment.java
+++ app/src/main/java/com/example/yummy/ui/view/near_by/NearByFragment.java
@@ -1,17 +1,36 @@
 package com.example.yummy.ui.view.near_by;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.example.yummy.R;
+import com.example.yummy.ui.view.viewmodel.MainActivityViewModel;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static android.content.Context.LOCATION_SERVICE;
 
 
-public class NearByFragment extends Fragment {
+public class NearByFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap map;
+    MainActivityViewModel mainActivityViewModel;
+
+
 
     public NearByFragment() {
     }
@@ -21,5 +40,29 @@ public class NearByFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_near_by, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mainActivityViewModel = new ViewModelProvider(getActivity()).get(MainActivityViewModel.class);
+        mainActivityViewModel.showBottomNavigation.setValue(false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fragment_map);
+
+
+        mapFragment.getMapAsync(this);
+    }
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        map = googleMap;
+        LatLng location = new LatLng(27.7172, 85.3240);
+
+        map.addMarker(new MarkerOptions().position(location).title("Location"));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(location,15));
     }
 }
